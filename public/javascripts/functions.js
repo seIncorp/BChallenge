0dfc8b3de6c03ea66381da65ed4a4c813b413efb
Simon <34153549+seIncorp@users.noreply.github.com>
2019-04-04 23:55:44 +0200

    Frontend photos 1.del

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,5 +1,5 @@
 let tempuser = {};
-let tempalbums = {};
+let tempalbum = {};
 
 
 function users(){
@@ -94,7 +94,9 @@ function *albumsCall(userId){
 
         if(obj.status == 'true')
         {
-            framework.create('label').body(`&nbsp;> ${tempuser.user.username}`).append('breadcrumbs');
+            
+            document.getElementById('breadcrumbs').innerHTML = `<label id="1" onclick="users()">users</label>`;
+            framework.create('label','2').body(`&nbsp;> ${tempuser.user.username}`).append('breadcrumbs');
 
             document.getElementById('main').innerHTML = '';
 
@@ -121,7 +123,7 @@ function *albumsCall(userId){
 
                 framework.create('tr')
                 .event("click", function(){
-                    photos(element.album.id);
+                    photos(element);
                 })
                 .body(
                     framework.create('td').body(element.album.title).element()
@@ -135,10 +137,54 @@ function *albumsCall(userId){
     });
 }
 
-function photos(a){
+function photos(element){
 
-   console.log('PPPPPPPPPP',a);
+    tempalbum = element;
+
+	let it;
+	let value;
+
+    it = photosCall(element.album.id);
+    value = it.next().value;
+    it.next(value);
 }
+
+function *photosCall(albumId){
+
+	let res = yield request({
+		type: 'POST', 
+		url: '/photos', 
+		dataR: JSON.stringify({
+            albumId: albumId
+        })
+    });
+
+    res.then( obj => {
+        console.log(obj);
+
+        if(obj.status == 'true')
+        {
+            framework.create('label','3').body(`&nbsp;> ${tempalbum.album.title}`).append('breadcrumbs');
+            
+            document.getElementById('2').onclick = function(){
+                albums(tempuser);
+            };
+
+            document.getElementById('main').innerHTML = '';
+
+            obj.data.forEach((element,id) => {
+                document.getElementById('main').innerHTML += `<img id="${id+'img'}" src="${element.thumbnailUrl}" onclick="">`;
+
+                let bb = document.getElementById(`${id+'img'}`);
+
+                document.getElementById('main').innerHTML += `<label style="position: absolute;left: ${bb.offsetLeft+'px'};top: ${(bb.offsetTop+bb.height-20)+'px'};color: black;">aa</label>`;
+            });
+        }
+    });
+}
+
+
+
 
 
 
